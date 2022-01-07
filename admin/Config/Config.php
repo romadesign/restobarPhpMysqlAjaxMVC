2a16f12a96481f99dfd7e787c1109ef228a7f3f2
romacode <roman247.rg@gmail.com>
2022-01-07 17:22:39 +0100

    mvc config - database conect - boostrap admin template

---
+++ admin/Config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+    const base_url = "http://localhost/proyect/restobarPhpMysqlAjaxMVC/admin/";
+    const host = "localhost";
+    const user = "root";
+    const password = "";
+    const db = "opd";
+    const charset = "charset=utf8";
+?>
---
+++ admin/Models/HomeModel.php
@@ -0,0 +1,8 @@
+<?php 
+    class HomeModel{
+        public function __construct()
+        {
+            //echo "conectar";
+        }
+    }
+?>
